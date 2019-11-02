00c52934538cbf79f83c09b5d8df294dfbffbfcf
Alessandro santos <lessandro-147@outlook.com>
2019-11-01 21:32:02 -0300

    altereiindex

--- AppOlaMundo/views.py
+++ AppOlaMundo/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from ProjetoOlaMundo.models import Paciente
+from django.views.generic.edit import CreateView
 
 class Index(TemplateView):
     template_name ="index.html"
+    
+class CriaFuncionario(CreateView): 
+    template_name = "index.html"   
+    model = Paciente()
+    fields = '__all__'
